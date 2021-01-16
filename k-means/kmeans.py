1aaf7568c522e225b55bdca5161cc4c35239bb9c
John Pesavento <54077093+jpes707@users.noreply.github.com>
2021-01-16 13:42:42 -0500

    Improved k-means

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -50,29 +50,30 @@ print('Most common pixel: {} => {}'.format(reversed_dictionary[reversed_key], re
 
 def run_k_means(k, pixel_weights):
     min_variance = float('inf')
-    best_means_set = set()
-    best_pixel_mapping = {}
     pixel_list = []
     for pixel in sorted(pixel_dictionary):
         pixel_list += [pixel] * pixel_dictionary[pixel]
     for _ in range(K_MEANS_TRIALS):
         # print('on pass', i)
-        means_set, pixel_mapping = single_k_means(k, pixel_list, pixel_weights)
+        means_set, pixel_mapping, iteration_count, old_means_set = single_k_means(k, pixel_list, pixel_weights)
         variance = calculate_variance(k, pixel_mapping)
         if variance < min_variance:
             min_variance = variance
             best_means_set = means_set
             best_pixel_mapping = pixel_mapping
-    return best_means_set, best_pixel_mapping
+            best_iteration_count = iteration_count
+            best_old_means_set = old_means_set
+    return best_means_set, best_pixel_mapping, best_iteration_count, best_old_means_set
 
 
 def single_k_means(k, pixel_list, pixel_weights):
-    means_set = set()
+    means_set = []
     for i in range(k):
-        new_pixel = pixel_list[random.randint(0, len(pixel_list) - 1)]
+        new_pixel = pixel_list[random.randint(0, len(pixel_list) - 1)]  # (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         while new_pixel in means_set:
             new_pixel = pixel_list[random.randint(0, len(pixel_list) - 1)]
-        means_set.add(new_pixel)
+        means_set.append(new_pixel)
+    original_means_set = means_set
     pixel_mapping = {}
     for pixel in pixel_weights:
         min_distance = float('inf')
@@ -94,9 +95,9 @@ def single_k_means(k, pixel_list, pixel_weights):
             for i in range(3):
                 means_average_dictionary[current_mean][0][i] += pixel[i] * pixel_count
             means_average_dictionary[current_mean][1] += pixel_count
-        means_set = set()
+        means_set = []
         for mean in means_average_dictionary:
-            means_set.add(tuple([int(means_average_dictionary[mean][0][i] / means_average_dictionary[mean][1]) for i in range(3)]))
+            means_set.append(tuple([int(means_average_dictionary[mean][0][i] / means_average_dictionary[mean][1]) for i in range(3)]))
         for pixel in pixel_weights:
             min_distance = float('inf')
             best_mean = ()
@@ -107,7 +108,7 @@ def single_k_means(k, pixel_list, pixel_weights):
                     best_mean = mean
             pixel_mapping[pixel] = best_mean
         if pixel_mapping == old_pixel_mapping:
-            return means_set, pixel_mapping
+            return means_set, pixel_mapping, iterations, original_means_set
         old_pixel_mapping = pixel_mapping.copy()
         pixel_mapping = {}
         iterations += 1
@@ -127,7 +128,7 @@ def calculate_distance(c1, c2):
     return math.sqrt(p)
 
 
-means_set, pixel_mapping = run_k_means(k, pixel_dictionary)
+means_set, pixel_mapping, iteration_count, old_means_set = run_k_means(k, pixel_dictionary)
 for x in range(img.size[0]):
     for y in range(img.size[1]):
         pix[x, y] = pixel_mapping[pix[x, y]]
@@ -136,8 +137,12 @@ sum_by_means = {mean: 0 for mean in means_set}
 for x in range(img.size[0]):
     for y in range(img.size[1]):
         sum_by_means[pix[x, y]] += 1
-print('Final means:')
-for idx, tup in enumerate(sorted([(sum_by_means[mean], mean) for mean in means_set], reverse=True)):
-    print('{}: {} => {}'.format(idx + 1, tup[1], tup[0]))
+print('Original means => final means => pixel counts:')
+for idx, tup in enumerate(sorted([(sum_by_means[mean], mean, old_means_set[mean_idx]) for mean_idx, mean in enumerate(means_set)], reverse=True)):
+    print('{}: {} => {} => {}'.format(idx + 1, tup[2], tup[1], tup[0]))
 img.save(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output.jpg'))
-print('Time elapsed: {}s'.format(adj_sig_figs(3, time() - start_time)))
+
+elapsed_time = time() - start_time
+print('Time elapsed: {} seconds'.format(adj_sig_figs(3, elapsed_time)))
+print('Number of steps: {} steps'.format(iteration_count))
+print('Rough time per step: {} steps/second'.format(adj_sig_figs(3, iteration_count / elapsed_time)))
